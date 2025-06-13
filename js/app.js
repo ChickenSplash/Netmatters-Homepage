a3d09666b5744f3a7366b389bbc4912392f45d4f
Emanuel Correia <guardeded@gmail.com>
2025-06-13 15:28:07 +0100

    finished sliders, started side bar

--- js/app.js
+++ js/app.js
@@ -4,6 +4,33 @@ menuToggleContainer.addEventListener('click', () => {
     menuToggle.classList.toggle('open');
 });
 
-$('.banner-carousel').slick({
+function initiateSliders() {
+  $('.banner-carousel').slick({
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 4300,
+      speed: 300,
+  })
+  
+  $('.slider-row').slick({
+    infinite: true,
     arrows: false,
-})
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 300,
+    variableWidth: true,
+    draggable: false,
+    centerMode: false,
+    responsive: [ // so it fixes bug on it disappearing on desktop view (desktop first)
+    {
+      breakpoint: 768, 
+      settings: {
+        centerMode: true,
+      }
+    }]
+  });
+}
+
+initiateSliders();
